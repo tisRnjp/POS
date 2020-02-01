e5c83e61f02ef495619912d73f53bc8d7c390a70
Ranjeep <ranjeep.maharjan@gmail.com>
2020-02-01 14:02:57 +0545

    Added banner model and seperated css

--- src/app/login/login-banner/login-banner.component.ts
+++ src/app/login/login-banner/login-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginBanner } from 'src/app/model/login-banner';
 
 @Component({
   selector: 'app-login-banner',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-banner.component.css']
 })
 export class LoginBannerComponent implements OnInit {
-
+  banner: LoginBanner = new LoginBanner(
+    "Best POS System in world!",
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet enim accusantium hic vitae odio numquam sunt consequuntur eaque a nostrum cupiditate fugiat suscipit tempore, deleniti necessitatibus ratione est earum id.",
+    "Easy to use. Easy to use."
+  );
   constructor() { }
 
   ngOnInit() {
