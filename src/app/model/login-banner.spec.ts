75faae8ba56b62b1134dc8f961b2a95dd61d6540
Ranjeep <ranjeep.maharjan@gmail.com>
2020-02-01 14:03:22 +0545

    addedd banner model

---
+++ src/app/model/login-banner.spec.ts
@@ -0,0 +1,7 @@
+import { LoginBanner } from './login-banner';
+
+describe('LoginBanner', () => {
+  it('should create an instance', () => {
+    expect(new LoginBanner()).toBeTruthy();
+  });
+});
---
+++ src/app/model/login-banner.ts
@@ -0,0 +1,7 @@
+export class LoginBanner {
+    constructor(
+        public MainTitle: string,
+        public Description: string,
+        public SubTitle?: string,
+    ){}
+}
